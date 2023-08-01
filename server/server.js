db9b1c0aeca606dd831ac120d7a8a919a43525f4
Leodan A Buduen <lbuduen@gmail.com>
2023-07-31 23:28:56 -0400

    [ADD] preparing for deployment at cyclic

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(express.static("client/build"));
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
   app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
   });
 }
 
